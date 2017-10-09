d8a7edf4808da5936e98efc43b8473106fbac486
Chris Jones <gwyn20@gmail.com>
2017-10-08 20:09:45 -0400

    Mentor Availability: Completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,5 +1,6 @@
 require 'httparty'
 require 'json'
+require 'byebug'
 
 class Kele
     include HTTParty
@@ -19,6 +20,7 @@ class Kele
     end
 
     def get_mentor_availability(mentor_id)
-        response = self.class.get('/mentors/#{mentor_id}/student_availability', headers: { "authorization" => @auth_token })
-        @mentor_availability = JSON.parse[response.body]
+        response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
+        @mentor_availability = JSON.parse(response.body)
+    end
 end

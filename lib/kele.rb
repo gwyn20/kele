e3cf18a1934af438c35aa6dbd94617a980becd62
Chris Jones <gwyn20@gmail.com>
2017-10-08 21:28:01 -0400

    Roadmaps and Checkpoints: Completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,9 +1,11 @@
 require 'httparty'
 require 'json'
 require 'byebug'
+require './lib/roadmap'
 
 class Kele
     include HTTParty
+    include Roadmap
     base_uri 'https://www.bloc.io/api/v1'
     
     def initialize(email, password)
